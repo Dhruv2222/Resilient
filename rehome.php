e6b8dfec69d3c1dea8b13ffd1edbcbdcf19a3869
Dhruv2222 <56965365+Dhruv2222@users.noreply.github.com>
2020-09-04 00:03:01 +0530

    Create rehome.php

---
+++ rehome.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+  <style>
+  body{
+    /* background-image: repeating-linear-gradient(white,#9933FF); */
+    background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSraJfiuK1417TXvBIDGlMSOWmmy33uevCEyw&usqp=CAU");
+  }
+  #main_heading{
+    font-family: "Sofia";
+    font-size: 25px;
+    font-weight: bold;
+  }
+      .card-body{
+          height: 200px;
+      }
+      #card-buttons{
+          width: 200px;
+      }
+  </style>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#" id="main_heading">Resilient</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Internships-offered</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Add-internship</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">TnC</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">About us</a>
+    </li>
+  </ul>
+  <form class="form-inline my-2 my-lg-0" >
+      <a class="nav-link disabled" href="#">My profile</a>
+      <img src="https://t4.ftcdn.net/jpg/00/64/67/63/240_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" class="rounded-circle" height="40px" width="40px" style="border-style:solid;">
+  </form>
+</div>
+</nav>
+<br>
+<center><h1 style="color:white;"> Welcome Recruiter!</h1></center>
+<br><br>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col">
+              <div class="card">
+          <img class="card-img-top px-5 py-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRiaoCpXqmdqHaNGtQMcl0eAmMmzWKe3vrF3w&usqp=CAU" height="200px" width="10px" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Add new internship</h5>
+            <p class="card-text">Post a new internship which will be visible to all interested students</p>
+            <center><a href="#" class="btn btn-primary" id="card-buttons">ADD NEW</a></center>
+          </div>
+        </div>
+            </div>
+            <div class="col">
+              <div class="card">
+          <img class="card-img-top" src="https://www.pngitem.com/pimgs/m/139-1398910_check-list-icon-green-png-transparent-png.png" height="200px" width="10px" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Internship-offered</h5>
+            <p class="card-text">Check out the internships you have offered</p><br>
+            <center><a href="#" class="btn btn-primary" id="card-buttons">OPEN</a></center>
+          </div>
+        </div>
+            </div>
+            <div class="col">
+              <div class="card">
+      <img class="card-img-top px-3" src="https://thumbs.dreamstime.com/b/group-people-unknown-personality-icon-digital-green-group-people-unknown-personality-icon-digital-green-any-114988548.jpg" height="200px" width="10px" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">View applicants</h5>
+        <p class="card-text">View all the applicants who have applied for internship at your organization</p>
+        <center><a href="#" class="btn btn-primary" id="card-buttons">VIEW</a></center>
+      </div>
+    </div>
+            </div>
+            <div class="col">
+              <div class="card">
+      <img class="card-img-top px-5 py-3" src="https://www.indianaintern.net/assets/images/icon--educators-bulb-@2x.png" height="200px" width="10px" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">All internships</h5>
+        <p class="card-text">Check out all the internships offered by other Recruiters</p><br>
+        <center><a href="#" class="btn btn-primary" id="card-buttons">VIEW</a></center>
+      </div>
+    </div>
+            </div>
+        </div>
+    </div>
+    
+    
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
