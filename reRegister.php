6e2f4bde2fad6c2cfd21bc1516221784dce9ac39
Dhruv2222 <56965365+Dhruv2222@users.noreply.github.com>
2020-09-12 14:34:37 +0530

    Update reRegister.php

--- reRegister.php
+++ reRegister.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <title>Add new Internship!</title>
+    <title>Registration</title>
     <style>
       body{
         /* background-image: repeating-linear-gradient(white,#9933FF); */
